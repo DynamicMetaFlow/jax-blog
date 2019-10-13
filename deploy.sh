a06fa3db13c898a4555ba716559b73568d0b935f
Alex <alexander.soto@gmail.com>
2019-10-13 17:04:58 -0400

    update

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ echo -e "\033[0;32mRunning hugo...\033[0m"
 hugo -d ~/www/jaxgil.github.io
 
 echo -e "\033[0;32mChanging to blog directory...\033[0m"
-cd ~/www/jaxgil.github.io
+cd ..jaxgil.github.io/
 
 echo -e "\033[0;32mCommit and push the new build...\033[0m"
 git commit -am "New Blog Build (`date`)"
