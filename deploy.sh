ed71316418afc0468ca404aa53a3ee7c5e9c2ff1
Alex <alexander.soto@gmail.com>
2019-10-13 17:18:27 -0400

    update

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,7 @@ git push
 echo -e "\033[0;32mDeploying new blog...\033[0m"
 
 echo -e "\033[0;32mDeleting old site...\033[0m"
-cd ../jaxgil.github.io
-rm -rf *
+rm -rf ~/www/jaxgil.github.io
 
 echo -e "\033[0;32mRunning hugo...\033[0m"
 snap run hugo -d ~/www/jaxgil.github.io
