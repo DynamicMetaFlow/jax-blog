bc2d16a7322a4b327a99a85c58cf0ac9a8273d52
Alex <alexander.soto@gmail.com>
2019-10-13 17:07:40 -0400

    update

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,13 @@ git push
 echo -e "\033[0;32mDeploying new blog...\033[0m"
 
 echo -e "\033[0;32mDeleting old site...\033[0m"
-rm -rf ~/www/jaxgil.github.io
+cd ~/www/jaxgil.github.io
+rm -rf *
+
 echo -e "\033[0;32mRunning hugo...\033[0m"
 hugo -d ~/www/jaxgil.github.io
 
 echo -e "\033[0;32mChanging to blog directory...\033[0m"
-cd ../jaxgil.github.io/
 
 echo -e "\033[0;32mCommit and push the new build...\033[0m"
 git commit -am "New Blog Build (`date`)"
